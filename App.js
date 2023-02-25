7e46c942f260433ab3a31ddd138a9762ff8a78c6
arjun-rawal <arawalarjun@gmail.com>
2023-02-25 11:59:22 -0500

    1

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
 import React from "react";
-import {Pressable, StyleSheet} from 'react-native'
-// 1. import `NativeBaseProvider` component
-import { NativeBaseProvider, Text, Box, Button, ColorMode, Image, Center } from "native-base";
+import { Pressable, StyleSheet, ScrollView } from "react-native";
+import {
+  Box,
+  Button,
+  Divider,
+  Heading,
+  HStack,
+  Icon,
+  List,
+  Switch,
+  Text,
+  Center,
+  useColorMode,
+  NativeBaseProvider,
+  IconButton,
+  VStack,
+} from "native-base";
+import HomeScreen from "./components/home.js";
+
 export default function App() {
-  
   return (
     <NativeBaseProvider>
-      <Box flex={1} bg="white" >
-          
-          <Pressable  style={styles.motivation} onPress={()=>{console.log("hello")}}>
-          <Image position='absolute' size={280} zIndex={1} alt="fallback text"  source={require("./assets/istockphoto-1255203350-612x612.jpg")} />
-      </Pressable>
-        </Box>
+      <HomeScreen/>
     </NativeBaseProvider>
   );
 }
 
-const styles= StyleSheet.create({
-  motivation:{
-    alignItems:'center',
-    position:'absolute',
-    paddingLeft: '100%',
-    paddingTop: '100%'
-  }
-})
+
