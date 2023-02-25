a561cb76d66171b44c8f4fb4d5b0d4a0388bbb1f
arjun-rawal <arawalarjun@gmail.com>
2023-02-25 13:52:08 -0500

    footer

--- App.js
+++ App.js
@@ -16,14 +16,153 @@ import {
   IconButton,
   VStack,
 } from "native-base";
-import HomeScreen from "./components/home.js";
+import { useState } from "react";
+
 
 export default function App() {
-  return (
-    <NativeBaseProvider>
-      <HomeScreen/>
-    </NativeBaseProvider>
-  );
+  const [screen, setScreen] = useState("Dashboard");
+
+const DisplayScreen = (screen) => {
+  if (screen.screen == "Dashboard"){
+    return <HomeScreen/>;
+  }
+  else if(screen.screen == "Water"){
+    return;
+  }
 }
 
 
+const Footer =() => {
+  return (
+    <Box bg="green.400">
+    <HStack space={2} justifyContent='center' >
+      <Button 
+                  w="100"
+                  h="50"
+                  bg="error.400"
+                  shadow={3}
+                  borderRadius={0}
+                  >
+        Home
+      </Button>
+      <Button 
+                  w="100"
+                  h="50"
+                  bg="error.400"
+                  shadow={3}
+                  borderRadius={0}
+                  >
+        Home
+      </Button>
+      <Button 
+                  w="100"
+                  h="50"
+                  bg="error.400"
+                  shadow={3}
+                  borderRadius={0}
+                  >
+        Home
+      </Button>
+    </HStack>
+    </Box>
+  );
+}
+
+const HomeScreen = () => {
+  return (
+    <ScrollView>
+      <VStack paddingTop={12} space={4} alignItems="center">
+        <HStack space={3} justifyContent="center">
+          <Button
+            _pressed={{ bg: "error.300" }}
+            _text={styles.buttonLabels}
+            w="175"
+            h="175"
+            bg="error.400"
+            rounded="lg"
+            shadow={3}
+          >
+            Motivation
+          </Button>
+          <Button
+            _pressed={{ bg: "info.300" }}
+            _text={styles.buttonLabels}
+            w="175"
+            h="175"
+            bg="info.500"
+            rounded="lg"
+            shadow={3}
+            onPress={()=>{setScreen("Water")}}
+          >
+            Water
+          </Button>
+        </HStack>
+
+        <HStack space={3} justifyContent="center">
+          <Button
+            _pressed={{ bg: "tertiary.100" }}
+            _text={styles.buttonLabels}
+            w="175"
+            h="175"
+            bg="tertiary.200"
+            rounded="lg"
+            shadow={3}
+          >
+            Breathe
+          </Button>
+          <Button
+            _pressed={{ bg: "fuchsia.200" }}
+            _text={styles.buttonLabels}
+            w="175"
+            h="175"
+            bg="fuchsia.300"
+            rounded="lg"
+            shadow={3}
+          >
+            Mind
+          </Button>
+        </HStack>
+
+        <HStack space={3} justifyContent="center">
+          <Button
+            _pressed={{ bg: "yellow.200" }}
+            _text={styles.buttonLabels}
+            w="175"
+            h="175"
+            bg="yellow.400"
+            rounded="lg"
+            shadow={3}
+          >
+            Nutrition
+          </Button>
+          <Button
+            _pressed={{ bg: "warmGray.400" }}
+            _text={styles.buttonLabels}
+            w="175"
+            h="175"
+            bg="blueGray.500"
+            rounded="lg"
+            shadow={3}
+          >
+            Focus
+          </Button>
+        </HStack>
+      </VStack>
+    </ScrollView>
+  );
+};
+return (
+  <NativeBaseProvider>
+    <DisplayScreen screen={screen}/>
+    <Footer />
+  </NativeBaseProvider>
+);
+
+}
+const styles = StyleSheet.create({
+  buttonLabels: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
