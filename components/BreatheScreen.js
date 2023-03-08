b60b68d00dd9d58216b966f0854e1df6f4ee30c8
arjun-rawal <arawalarjun@gmail.com>
2023-03-07 20:40:26 -0500

    added loading to first breathe component

--- components/BreatheScreen.js
+++ components/BreatheScreen.js
@@ -1,12 +1,12 @@
-import {Image,Center,Box,Slide,Button} from "native-base"
-import {Text} from "react-native"
-import {useState} from 'react'
-export default function BreatheScreen(){
-  const [index,setIndex] = useState(0)
-  const breathingTypes = ["box","asd"];
-     return(
-       <>
-       <Box
+import { Image, Center, Box, Slide, Button, Spinner } from "native-base";
+import { Text } from "react-native";
+import { useState } from "react";
+export default function BreatheScreen() {
+  const [index, setIndex] = useState(0);
+  const breathingTypes = ["box", "asd"];
+  return (
+    <>
+      <Box
         padding={4}
         style={{
           flex: 1,
@@ -14,30 +14,44 @@ export default function BreatheScreen(){
           justifyContent: "center",
           alignItems: "center",
         }}
->
-   
-
+      >
         <BreathingTechnique type={breathingTypes[index]} />
-        <Button marginTop={3} size="md"onPress={()=>{setIndex(index+1)}}> Next </Button>
-
-    </Box>
-
+        <Button
+          marginTop={3}
+          size="lg"
+          onPress={() => {
+            setIndex(index + 1);
+          }}
+        >
+          Next
+        </Button>
+      </Box>
     </>
-     );
+  );
 }
 
+const BreathingTechnique = ({ type }) => {
+  const [imageLoaded, setImageLoaded] = useState("block");
+  switch (type) {
+    case "box":
+      return (
+        <Center _text={{ fontSize: 21, fontWeight: "bold" }}>
+          Box Breathing
+          <Spinner style={{ display: imageLoaded }} />
+          <Image
+            source={{
+              uri: "https://blog.noisli.com/wp-content/uploads/2022/07/box-breathing-technique.gif",
+            }}
+            alt="Alternate Text"
+            size="2xl"
+            onLoad={() => {
+              setImageLoaded("none");
+            }}
+          />
+        </Center>
+      );
 
-  const BreathingTechnique =({type})=>{
-  switch (type){
-    case "box":return (<Center _text={{fontSize: 21,fontWeight: "bold",
-        }}>
-    Box Breathing
-    <Image source={{
-      uri: "https://blog.noisli.com/wp-content/uploads/2022/07/box-breathing-technique.gif"
-    }} alt="Alternate Text" size="2xl" />
-    </Center>)
-
-    case "asd" : return(<Text>hello</Text>)
+    case "asd":
+      return <Text>hello</Text>;
   }
-  
-}
+};
