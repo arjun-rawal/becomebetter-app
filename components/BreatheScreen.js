f6ca13c7b353b373b81d8ecee454a1f0e874cc2e
arjun-rawal <arawalarjun@gmail.com>
2023-03-08 21:11:40 -0500

    added breathing techniques
    4-7-8, finger, partial balloon
    issue where gifs don't load in time when going next and back

--- components/BreatheScreen.js
+++ components/BreatheScreen.js
@@ -1,9 +1,9 @@
-import { Image, Center, Box, Slide, Button, Spinner } from "native-base";
+import { Image, Center, Box, Slide, Button, Spinner, HStack } from "native-base";
 import { Text } from "react-native";
 import { useState } from "react";
 export default function BreatheScreen() {
   const [index, setIndex] = useState(0);
-  const breathingTypes = ["box", "asd"];
+  const breathingTypes = ["box", "4-7-8","Finger"];
   return (
     <>
       <Box
@@ -16,6 +16,9 @@ export default function BreatheScreen() {
         }}
       >
         <BreathingTechnique type={breathingTypes[index]} />
+      <HStack space={3}>
+      {index <breathingTypes.length-1
+      &&
         <Button
           marginTop={3}
           size="lg"
@@ -23,8 +26,21 @@ export default function BreatheScreen() {
             setIndex(index + 1);
           }}
         >
+      
           Next
         </Button>
+      }
+        {index >=1 &&
+        <Button 
+          marginTop={3}
+          size="lg"
+          onPress={() => {
+            setIndex(index - 1);
+          }}> 
+          Back
+          </Button>
+        }
+      </HStack>
       </Box>
     </>
   );
@@ -51,7 +67,56 @@ const BreathingTechnique = ({ type }) => {
         </Center>
       );
 
-    case "asd":
-      return <Text>hello</Text>;
+    case "4-7-8":
+      return (
+        <Center _text={{ fontSize: 21, fontWeight: "bold" }}>
+          4-7-8
+          <Spinner style={{ display: imageLoaded }} />
+          <Image
+            source={{
+              uri: "https://i.pinimg.com/originals/a4/ba/fc/a4bafc9008968c4dcf8df311f2c26802.gif",
+            }}
+            alt="Alternate Text"
+            size="2xl"
+            onLoad={() => {
+              setImageLoaded("none");
+            }}
+          />
+        </Center>
+      );
+    case "Finger":
+    return(
+        <Center _text={{ fontSize: 21, fontWeight: "bold" }}>
+          Finger Breathing
+          <Spinner style={{ display: imageLoaded }} />
+          <Image
+            source={{
+              uri: "https://d2r91rtd4f6vua.cloudfront.net/content/2022/04/13/MTV_PauseforFiveFinger-Breathing_DR.gif",
+            }}
+            alt="Alternate Text"
+            size="2xl"
+            onLoad={() => {
+              setImageLoaded("none");
+            }}
+          />
+        </Center>
+    )
+    case "Balloon":
+        return(
+<Center _text={{ fontSize: 21, fontWeight: "bold" }}>
+          Balloon breaths
+          <Spinner style={{ display: imageLoaded }} />
+          <Image
+            source={{
+              uri: "https://static.wixstatic.com/media/5b54bd_c8067e085f8b4767a268bb0abbe5972d.gif",
+            }}
+            alt="Alternate Text"
+            size="2xl"
+            onLoad={() => {
+              setImageLoaded("none");
+            }}
+          />
+        </Center>
+        )
   }
 };
